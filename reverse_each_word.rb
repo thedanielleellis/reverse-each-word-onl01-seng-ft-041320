14a8cf66b5de83dd2998661738ff159fc8e69b75
Danielle Ellis <thedanielleellis@gmail.com>
2020-04-17 18:49:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,6 @@
-reverse_each_word("word")
+def reverse_each_word(sentence)
+  words_array = sentence.split(" ")
+  new_array = []
+  new_array = words_array.collect {|word| word.reverse }
+  new_array.join(" ")
+end
